b4191dcc14c81efd9c90f472bc3caa3bd026ad3e
Jacob <jmodonnell96@gmail.com>
2024-06-30 09:09:24 -0500

    Fixed opening php tag

--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -16,7 +16,7 @@ while (have_posts()) {
         <a class="metabox__blog-home-link" href="<?= get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span class="metabox__main"><?php the_title() ?></span>
       </p>
     </div>
-  <? }
+  <?php }
   ?>
 
 
